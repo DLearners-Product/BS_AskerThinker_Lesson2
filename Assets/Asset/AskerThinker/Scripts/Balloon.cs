108572bb803a79cbb7718e28987437d229380350
karthickdl <karthick@dlearners.in>
2023-01-28 19:06:25 +0530

    Daily commit 28/01/2023

--- Assets/Asset/AskerThinker/Scripts/Balloon.cs
+++ Assets/Asset/AskerThinker/Scripts/Balloon.cs
@@ -1,18 +1,34 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
-
 
 public class Balloon : MonoBehaviour
 {
+    //audio
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip balloonFly;
+    [SerializeField] private AudioClip whoosh;
+
     //anim
     [SerializeField] private Animator balloonAnim;
 
+    //inputfield
     [SerializeField] private TMP_InputField answerInputField;
+
+    private void Start()
+    {
+        PlaySE(balloonFly);
+    }
 
     public void KeepFloating()
     {
         balloonAnim.SetTrigger("KeepFloating");
         answerInputField.gameObject.SetActive(true);
+        PlaySE(whoosh);
+    }
+
+    public void PlaySE(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
     }
 }
--- Assets/Asset/AskerThinker/Scripts/GuessingActivity.cs
+++ Assets/Asset/AskerThinker/Scripts/GuessingActivity.cs
@@ -7,7 +7,10 @@ using TMPro;
 public class GuessingActivity : MonoBehaviour
 {
     //audio
-
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip fall;
+    //[SerializeField] private AudioClip whoosh;
+    [SerializeField] private AudioClip balloonPop;
 
     //text
     [SerializeField] private TextMeshProUGUI[] guessAnswers;
@@ -19,13 +22,18 @@ public class GuessingActivity : MonoBehaviour
     [SerializeField] private Button btnSubmit;
 
     //GO
+    [SerializeField] private GameObject saved;
+    [SerializeField] private GameObject popTheBalloon;
     [SerializeField] private GameObject[] questionBalloons;
 
     private int i = 0;
+    private Dictionary<int, string> childsGuessAnswers;
 
     private void Start()
     {
         guessInputField[i].onValueChanged.AddListener(delegate { OnGuessEnteredCheck(); });
+
+        childsGuessAnswers = new Dictionary<int, string>();
     }
 
     public void OnGuessEnteredCheck()
@@ -42,13 +50,55 @@ public class GuessingActivity : MonoBehaviour
 
     public void OnClickSubmitButton()
     {
-        //store the answer, show answer stored confirmation to child, ask child to pop the balloon
+        //store the answer
+        childsGuessAnswers.Add(i + 1, guessAnswers[i].text);
+
+        saved.SetActive(true);
+
+        //show answer stored confirmation to child
+        Invoke("Save", 1f);
 
 
-
-        i++;
     }
 
+    public void Save()
+    {
+        guessInputField[i].GetComponent<Animator>().SetTrigger("Disappear");
 
+        //ask child to pop the balloon
+        popTheBalloon.SetActive(true);
+    }
 
+    public void OnClickBalloon()
+    {
+        //balloon pop
+        PlaySE(balloonPop);
+        popTheBalloon.SetActive(false);
+
+        //play balloon pop animation
+        questionBalloons[i].GetComponent<Animator>().SetTrigger("Pop");
+
+        Invoke("QuestionFalling", 0.5f);
+    }
+
+    public void QuestionFalling()
+    {
+        //paper falling
+        questionBalloons[i].transform.GetChild(0).GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+
+        Invoke("SpawnNextBalloon", 2.5f);
+    }
+
+    public void SpawnNextBalloon()
+    {
+        i++;
+        questionBalloons[i].SetActive(true);
+        PlaySE(fall);
+    }
+
+    public void PlaySE(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
 }
