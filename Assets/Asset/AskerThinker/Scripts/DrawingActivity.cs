1d72d983381c2029f21f8fe85131297c0db43c65
karthickdl <karthick@dlearners.in>
2023-01-31 19:29:46 +0530

    Daily commit 31/01/2023

--- Assets/Asset/AskerThinker/Scripts/DrawingActivity.cs
+++ Assets/Asset/AskerThinker/Scripts/DrawingActivity.cs
@@ -4,24 +4,74 @@ using UnityEngine;
 
 public class DrawingActivity : MonoBehaviour
 {
-    public const float RESOLUTION = 0.1f;
-    //line
-    [SerializeField] private Camera _camera;
-    [SerializeField] private Line _linePrefab;
-    private Line _currentLine;
+    public Camera camera;
+    public GameObject brush;
+
+    LineRenderer currentLineRenderer;
+    Vector2 lastPos;
+
+
+    //audio
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip paintApply;
+
+    //texture
+    [SerializeField] private Texture2D[] brushTexture;
+
+    //color
+    [SerializeField] private Color[] colorList;
+
+    private Color curColor;
+    private int colorCount;
+
+
+    private void Start()
+    {
+        //setting default violet color brush
+        //brushTexture[8] is the default black color paint bucket cursor
+        Cursor.SetCursor(brushTexture[3], new Vector2(brushTexture[0].width / 8, brushTexture[0].height), CursorMode.ForceSoftware);
+
+    }
 
 
     private void Update()
     {
-        Vector2 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
-
-        if (Input.GetMouseButtonDown(0))
-            _currentLine = Instantiate(_linePrefab, mousePos, Quaternion.identity);
-
-
-        if (Input.GetMouseButton(0))
-            _currentLine.SetPosition(mousePos);
-
-
+        Draw();
     }
+
+    void Draw()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            CreateBrush();
+        }
+        else if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+            if (mousePos != lastPos)
+            {
+                AddAPoint(mousePos);
+                lastPos = mousePos;
+            }
+        }
+    }
+
+    void CreateBrush()
+    {
+        GameObject brushInstance = Instantiate(brush);
+        currentLineRenderer = brushInstance.GetComponent<LineRenderer>();
+
+        Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+
+        currentLineRenderer.SetPosition(0, mousePos);
+        currentLineRenderer.SetPosition(1, mousePos);
+    }
+
+    void AddAPoint(Vector2 pointPos)
+    {
+        currentLineRenderer.positionCount++;
+        int positionIndex = currentLineRenderer.positionCount - 1;
+        currentLineRenderer.SetPosition(positionIndex, pointPos);
+    }
+
 }
