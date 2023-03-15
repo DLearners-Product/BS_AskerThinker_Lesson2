27dca75b85b8511e56ec69f71c713d6a5528756f
karthickdl <karthick@dlearners.in>
2023-03-15 19:31:46 +0530

    Daily commit 15/03/2023

---
+++ Assets/Asset/AskerThinker/Scripts/StoryController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class StoryController : MonoBehaviour
+{
+    //video
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    //go
+    [SerializeField] private GameObject blackScreen;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += CheckOver;
+    }
+
+    public void CheckOver(VideoPlayer vp)
+    {
+        Invoke("ShowBlackScreen", 1f);
+    }
+
+    public void ShowBlackScreen()
+    {
+        blackScreen.SetActive(true);
+    }
+
+
+
+
+}
